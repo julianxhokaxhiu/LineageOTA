560f1a2247e3870fa88abeee441088cff2642006
androidmeda <hephappy@gmail.com>
2014-05-27 23:55:03 +0300

    Not only cm, but also product...
    Change-Id: I0cb0bd04015777dc2274eeed0804e50069a8bb6d

--- app/Tokens.php
+++ app/Tokens.php
@@ -51,7 +51,7 @@
             $cache = $mc->get($filePath);
             if (!$cache && Memcached::RES_NOTFOUND == $mc->getResultCode()) {
                 $buildpropArray = explode("\n", file_get_contents('zip://'.$filePath.'#system/build.prop'));
-                if ($device == $this->getBuildPropValue($buildpropArray, 'ro.cm.device')) {
+                if ($device == $this->getBuildPropValue($buildpropArray, 'ro.product.device')) {
                     $api_level = intval($this->getBuildPropValue($buildpropArray, 'ro.build.version.sdk'));
                     $incremental = $this->getBuildPropValue($buildpropArray, 'ro.build.version.incremental');
                     $timestamp = intval($this->getBuildPropValue($buildpropArray, 'ro.build.date.utc'));
