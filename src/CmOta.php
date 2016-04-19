beab43c1e321a89d4a487eecf21135077cc741a8
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2016-04-19 08:37:45 +0200

    Update LICENSE year

--- src/CmOta.php
+++ src/CmOta.php
@@ -2,7 +2,7 @@
     /*
         The MIT License (MIT)
 
-        Copyright (c) 2014 Julian Xhokaxhiu
+        Copyright (c) 2016 Julian Xhokaxhiu
 
         Permission is hereby granted, free of charge, to any person obtaining a copy of
         this software and associated documentation files (the "Software"), to deal in
