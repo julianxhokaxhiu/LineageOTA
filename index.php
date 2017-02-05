f329c40ec1a8b6f6281f830a2ad6105f21881230
Brian J. Murrell <brian@interlinx.bc.ca>
2017-02-05 09:37:09 -0500

    Automatically support having a path to the server
    If the user has installed the server at a path other than the root of their server, figure that out and initialise basePath with it.

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 
     $app = new CmOta();
     $app
-    ->setConfig( 'basePath', 'http://'.$_SERVER['HTTP_HOST'] )
+    ->setConfig( 'basePath', 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']) )
     ->setConfig( 'memcached.host', 'localhost' )
     ->setConfig( 'memcached.port', 11211 )
     ->enableMemcached()
