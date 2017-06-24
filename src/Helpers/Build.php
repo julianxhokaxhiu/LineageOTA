3acefcb1df9904f52260577a2f76cbc42f14e6ff
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-06-24 14:30:55 +0200

    Use the internal ROM timestamp from build.prop
    Fixes #30

--- src/Helpers/Build.php
+++ src/Helpers/Build.php
@@ -65,7 +65,7 @@
             $this->filePath = $physicalPath . '/' . $fileName;
             $this->channel = $this->_getChannel( str_replace( range( 0 , 9 ), '', $tokens[4] ), $tokens[1], $tokens[2] );
             $this->filename = $fileName;
-            $this->timestamp = filemtime( $this->filePath );
+            $this->timestamp = $this->getBuildPropValue( 'ro.build.date.utc' );
             $this->buildProp = explode( "\n", @file_get_contents('zip://'.$this->filePath.'#system/build.prop') );
             $this->incremental = $this->getBuildPropValue( 'ro.build.version.incremental' );
             $this->apiLevel = $this->getBuildPropValue( 'ro.build.version.sdk' );
